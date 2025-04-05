492e9eb3a7212e775710edd0e297ab4fa84369d2
Iden Ticlla <iden.ticlla@gmail.com>
2025-04-04 22:31:31 -0400

    Add assistant retrieval and deletion endpoints; refactor model import for consistency

--- backend/routes/assistant.py
+++ backend/routes/assistant.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, status, Depends, HTTPException
-from models.assistant import Assistant
+from models.assistant import Assistant as AssistantModel
 from schemas.assistant import AssistantCreate, Assistant as AssistantSchema
 from sqlalchemy.orm import Session
 from db.session import get_db
 
 
 router = APIRouter(
-    prefix='/assistants',
-    tags=['Assistants']    
+    prefix="/assistants",
+    tags=["Assistants"]
 )
 
 @router.get(
@@ -18,8 +18,31 @@ router = APIRouter(
     description="This endpoint is used to see all the assistants on the platform."
 )
 def get_all_assistants(db: Session = Depends(get_db)):
-    assistants = db.query(Assistant).all()
+    assistants = db.query(AssistantModel).all()
     return assistants
+
+@router.get("/{assistant_id}", response_model=AssistantSchema)
+async def get_assistant(assistant_id: int, db: Session = Depends(get_db)):
+    assistant = db.query(AssistantModel).filter(AssistantModel.id == assistant_id).first()
+    if not assistant:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Assistant with id {assistant_id} not found"
+        )
+    return assistant
+
+@router.delete("/{assistant_id}", response_model=AssistantSchema)
+async def delete_assistant(assistant_id:int, db: Session = Depends(get_db)):
+    db_assistant = db.query(AssistantModel).filter(AssistantModel.id == assistant_id).first()
+    if not db_assistant:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Assistant with id {assistant_id} not found"
+        )
+    db.delete(db_assistant)
+    db.commit()
+    return db_assistant
+
 
 @router.post(
     '',
@@ -29,13 +52,13 @@ def get_all_assistants(db: Session = Depends(get_db)):
     description="This endpoint is used to create a new trip assistant."
 )
 def create_assistant(assistant: AssistantCreate, db: Session = Depends(get_db)):
-    existing_assistant = db.query(Assistant).filter(Assistant.phone_number == assistant.phone_number).first()
+    existing_assistant = db.query(AssistantModel).filter(AssistantModel.phone_number == assistant.phone_number).first()
     if existing_assistant:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="Phone number already exists."
         )
-    assistant = Assistant(**assistant.model_dump())
+    assistant = AssistantModel(**assistant.model_dump())
     db.add(assistant)
     db.commit()
     db.refresh(assistant)
